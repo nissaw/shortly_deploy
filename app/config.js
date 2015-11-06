a7da26ff4982f56f23950694d6c5e35ef15bb23b
nissaw <nissa.wollum@gmail.com>
2015-11-05 19:41:16 -0800

    Mongo Refactor

--- app/config.js
+++ app/config.js
@@ -1,10 +1,11 @@
-var Bookshelf = require('bookshelf');
-var path = require('path');
-
 var mongoose = require ('mongoose');
 
 
+mongoose.connect('mongodb://localhost/shortlydb');
 
+var db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', console.log('Connected Mongoose'));
 
 module.exports = db;
